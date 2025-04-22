eb65a39d18a8a1d722c577c8b835e594358f08a6
AlexMao84 <gotics@gmail.com>
2025-04-22 13:01:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,20 @@
-const CACHE_NAME = 'cotizador-apu-v1';
-const urlsToCache = [
-    '/cotizador-apu/',
-    '/cotizador-apu/index.html',
-    '/cotizador-apu/manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js',
-    'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
-];
-
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache abierto');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('cotizador-apu-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js'
+            ]);
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
