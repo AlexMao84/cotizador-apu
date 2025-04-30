9d2388235e0416dbe4867792974212631d36a006
AlexMao84 <gotics@gmail.com>
2025-04-30 15:09:29 -0500

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,2 @@
+const { createClient } = Supabase;
+const supabase = createClient('https://ufqrenidgxajhndkoiei.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmcXJlbmlkZ3hhamhuZGtvaWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNDI0ODMsImV4cCI6MjA2MTYxODQ4M30.GukxdkbSv-J3lWRLLDwLrBtoeUI2VI56COsM35iREqo');
