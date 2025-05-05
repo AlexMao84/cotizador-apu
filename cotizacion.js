129742b766848a630c245f39396f60d84c4d4a03
AlexMao84 <gotics@gmail.com>
2025-05-05 14:38:44 -0500

    Update cotizacion.js

--- cotizacion.js
+++ cotizacion.js
@@ -1,8 +1,8 @@
-const GITHUB_TOKEN = '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'; // Reemplaza con tu token de GitHub
-const REPO_OWNER = 'alexmao84'; // Reemplaza con tu usuario de GitHub
-const REPO_NAME = 'cotizador-apu'; // Reemplaza con el nombre de tu repositorio
-const COTIZACIONES_JSON_PATH = 'cotizaciones.json'; // Ruta del archivo JSON en el repositorio
-const PDF_FOLDER = 'cotizaciones_pdf'; // Carpeta para los PDFs en el repositorio
+const GITHUB_TOKEN = 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; // Reemplaza con tu token de GitHub
+const REPO_OWNER = alexmao84; // Reemplaza con tu usuario de GitHub
+const REPO_NAME = cotizador-apu; // Reemplaza con el nombre de tu repositorio
+const COTIZACIONES_JSON_PATH = cotizaciones.json; // Ruta del archivo JSON en el repositorio
+const PDF_FOLDER = cotizaciones_pdf; // Carpeta para los PDFs en el repositorio
 
 async function fetchCotizacionesFromGitHub() {
     try {
